30c44484122832ecc6530f99464d73ebac9e9310
VARSHITH MOHAN G <95514418+iamvarshith45@users.noreply.github.com>
2025-07-22 15:30:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MLops-AWS',
+    version= '0.0.0',
+    author= 'varshith',
+    author_email= 'iamvarshith45@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
